1cb260095d9b9a572c6d1393ec70cbbe9b51a03d
liuyq <liuyiqi24@mails.tsinghua.edu.cn>
2026-01-07 15:55:00 +0800

    core: expose chi square functions

--- core/__init__.py
+++ core/__init__.py
@@ -6,5 +6,16 @@ from .utils import (
     isParamsWithinConstraints,
     merge_bins,
     compute_init,
+    merged_pearson_chi2,
+    modified_neyman_chi2_A,
+    modified_neyman_chi2_B,
+    mighell_chi2,
 )
 from .fft_utils import fft_and_ifft, roll_and_pad
+
+__all__ = [
+    "merged_pearson_chi2",
+    "modified_neyman_chi2_A",
+    "modified_neyman_chi2_B",
+    "mighell_chi2",
+]
